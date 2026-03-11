04bebbc4db3043f0ad7d59380020d23b99ab9f58
Tymur <nsdfd36@gmail.com>
2026-03-11 17:09:02 +0200

    Implemented basic ACORN PRNG

---
+++ src/generator.ts
@@ -0,0 +1,37 @@
+const GeneratorFunction = function* () {}.constructor;
+
+// "ACORN" random number generator
+// https://grokipedia.com/page/acorn_random_number_generator
+const M = 2 ** 30; // modulus; increase the degree for more precision
+const maxOrder = 10; // increase for more precision
+// const maxOP1 = maxOrder + 1;
+const baseSeed = 1234567; // only odd numbers > 100k should be used, small first elements otherwise
+
+const seedsOne: number[] = [];
+for (let x = 0; x < maxOrder; x++) {
+    seedsOne.push(baseSeed + x * 2); // multiply by 2 to ensure odd numbers
+}
+const seedsTwo: number[] = [];
+for (let x = 0; x < maxOrder; x++) {
+    seedsTwo.push(baseSeed + x * 2);
+}
+function* GenerateSeed() 
+{
+    for (let x = 0; x < maxOrder; x++) 
+    {
+        seedsOne[x] = (seedsOne[x] + seedsTwo[x]);
+        seedsTwo[x] = (seedsTwo[x] + seedsOne[x]);
+        if (seedsTwo[x] >= M) 
+        {
+            seedsTwo[x] -= M;
+            seedsOne[x] += 1;
+        }
+        if (seedsOne[x] >= M) 
+        {
+            seedsOne[x] -= M;
+        }
+    }
+    let randomNumber = (seedsOne[maxOrder - 1] + seedsTwo[maxOrder - 1] / M) / M;
+
+    yield randomNumber;
+}
--- src/generators.ts
+++
@@ -1,2 +0,0 @@
-const GeneratorFunction = function* () {}.constructor;
-
