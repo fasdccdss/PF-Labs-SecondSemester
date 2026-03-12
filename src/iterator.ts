c6e26050047cf1cafe275a83a15c08b9d5177d00
Tymur <nsdfd36@gmail.com>
2026-03-12 18:00:39 +0200

    added basic iterator

--- src/iterator.ts
+++ src/iterator.ts
@@ -3,7 +3,11 @@ function TimeoutIterator(iterator: Iterator<number>, timeoutSeconds: number)
     let start = Date.now();
     let timeout = timeoutSeconds * 1000; // convert to milliseconds
 
-    while (Date.now() - start < timeout) {
-        
+    while (Date.now() - start < timeout)
+    {
+        const result = iterator.next();
+        if (result.done) break; 
+
+        console.log(result.value);
     }
 }
