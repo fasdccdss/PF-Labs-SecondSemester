ce12f593543c8beb9fa32530bb2960757bb7d36a
Tymur <nsdfd36@gmail.com>
2026-04-17 17:11:25 +0300

    added retrieve methods

--- src/priorityQueue.ts
+++ src/priorityQueue.ts
@@ -4,6 +4,9 @@ const priorityBins = new Map<Priority, Map<number, Entry<any>>>();
 const allEntries = new Map<number, Entry<any>>();
 
 /* FUNCTIONS */
+function generateKey(): number {
+    return ++keyCounter;
+}
 function Enqueue<T>(value: T, priority: Priority) 
 {
     const key = generateKey();
@@ -14,23 +17,43 @@ function Enqueue<T>(value: T, priority: Priority)
     priorityBins.get(priority)!.set(key, entry);
     allEntries.set(key, entry);
 }
-function Dequeue(retrieveMode: RetrieveMode) 
+function Dequeue(retrieveHighestPriority: boolean, retrieveOldestEntry: boolean) 
 {
-    switch (retrieveMode) 
-    {
 
-    }
 }
-function Peek(retrieveMode: RetrieveMode)
+function Peek(retrieveHighestPriority: boolean, retrieveOldestEntry: boolean)
 {
-    switch (retrieveMode) 
-    {
+    // retrieveHighestPriority?
+    // retrieveOldest?
 
-    }
 }
 
-function generateKey(): number {
-    return ++keyCounter;
+function RetrievePriorityMap(retrieveHighest: boolean): number 
+{
+    let priorityKey: Priority | undefined;
+
+    for (const priority of priorityBins.keys())
+    {
+        if (priorityKey === undefined) priorityKey = priority;
+        else if (retrieveHighest && priority < priorityKey) priorityKey = priority;
+        else if (!retrieveHighest && priority > priorityKey) priorityKey = priority;
+    }
+    return priorityKey!;
+}
+function RetrieveMapEntry(priorityKey: number, retrieveOldest: boolean): number
+{
+    const bin = priorityBins.get(priorityKey)!;
+
+    if (retrieveOldest)
+    {
+        return bin.keys().next().value;
+    }
+    else 
+    {
+        let lastKey!: number;
+        for (const key of bin.keys()) lastKey = key;
+        return lastKey;
+    }
 }
 
 /* TYPES */
@@ -45,10 +68,14 @@ enum Priority
 {
     S, A, B, C, D, E, F
 }
-enum RetrieveMode
+/*
+enum RetrievePriority
 {
     highest,
-    lowest,
+    lowest
+}
+enum RetrieveMode {
     oldest,
     newest
 }
+*/
