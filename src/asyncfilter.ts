8be3c65077fd8296400d71d24fecd3bfeb6895f2
Tymur <nsdfd36@gmail.com>
2026-04-18 00:06:37 +0300

    added type safety guards + started on callback async filter

--- src/asyncfilter.ts
+++ src/asyncfilter.ts
@@ -4,9 +4,24 @@
 // We write our own array.filter function that works in async 
 // instead of trying to convert the original array.fiter function
 /////////////
-async function FFFF( array: Array<any>, checkMethod: (item: any) => Promise<boolean> )
+/* PROMISE */
+async function asyncFilterPromise<T>( array: Array<T>, checkMethod: (item: T) => Promise<boolean> )
 {
     const results = await Promise.all(array.map((item) => checkMethod(item)));
 
-    return array.filter((item, index) => results[index]);
+    return array.filter((_blank, index) => results[index]);
 }
+
+// Usage example:
+
+/* CALLBACK */
+function asyncFilterCallback<T> ( 
+    array: Array<T>,
+    checkMethod: (item: T) => Promise<boolean>,
+    callback: (error: Error | null, result: Array<T> | null) => void
+)
+{
+
+}
+
+// Usage example:
