61cd03e62e5b1bc4084f560cc92ec040301f291c
Tymur <nsdfd36@gmail.com>
2026-04-18 00:22:34 +0300

    implemented asyncFilterCallback

--- src/asyncfilter.ts
+++ src/asyncfilter.ts
@@ -4,10 +4,14 @@
 // We write our own array.filter function that works in async 
 // instead of trying to convert the original array.fiter function
 /////////////
+
+type AsyncPredicate<T> = (item: T) => Promise<boolean>;
+type Callback<T> = (error: Error | null, result: Array<T> | null) => void
+
 /* PROMISE */
-async function asyncFilterPromise<T>( array: Array<T>, checkMethod: (item: T) => Promise<boolean> )
+async function asyncFilterPromise<T>(array: Array<T>, asyncPredicate: AsyncPredicate<T>)
 {
-    const results = await Promise.all(array.map((item) => checkMethod(item)));
+    const results = await Promise.all(array.map((item) => asyncPredicate(item)));
 
     return array.filter((_blank, index) => results[index]);
 }
@@ -17,11 +21,15 @@ async function asyncFilterPromise<T>( array: Array<T>, checkMethod: (item: T) =>
 /* CALLBACK */
 function asyncFilterCallback<T> ( 
     array: Array<T>,
-    checkMethod: (item: T) => Promise<boolean>,
-    callback: (error: Error | null, result: Array<T> | null) => void
-)
+    asyncPredicate: AsyncPredicate<T>,
+    callback: Callback<T> 
+) 
 {
-
+    Promise.all(array.map((item) => asyncPredicate(item)))
+    .then(results => {
+        const filtered = array.filter((_blank, index) => results[index]);
+        callback(null, filtered);
+    }).catch(err => callback(err, null))
 }
 
 // Usage example:
