bd9dbbee34a473bb0c8227fc464b3c4f44ec2b73
Tymur <nsdfd36@gmail.com>
2026-04-17 15:18:01 +0300

    added Enqueue

--- src/priorityQueue.ts
+++ src/priorityQueue.ts
@@ -1,26 +1,51 @@
-const globalMap = Map<string, Map<Priority, any>>;
+let keyCounter = 0;
 
-function Enqueue() 
-{
+const priorityBins = new Map<Priority, Map<number, Entry<any>>>();
+const allEntries = new Map<number, Entry<any>>();
 
-}
-function Dequeue() 
+/* FUNCTIONS */
+function Enqueue<T>(value: T, priority: Priority) 
 {
+    const key = generateKey();
+    const entry = { key, value, priority };
 
-}
-function Peek(retrieve: RetrieveOptions)
-{
+    if (!priorityBins.has(priority)) priorityBins.set(priority, new Map());
     
+    priorityBins.get(priority)!.set(key, entry);
+    allEntries.set(key, entry);
+}
+function Dequeue(retrieveMode: RetrieveMode) 
+{
+    switch (retrieveMode) 
+    {
+
+    }
+}
+function Peek(retrieveMode: RetrieveMode)
+{
+    switch (retrieveMode) 
+    {
+
+    }
 }
 
-// type GlobalMap
-// PriorityMap: new Map<Priority, any>;
+function generateKey(): number {
+    return ++keyCounter;
+}
 
+/* TYPES */
+type Entry<T = any> = {
+    key: number;
+    value: T;
+    priority: Priority;
+};
+
+/* ENUMS */
 enum Priority 
 {
     S, A, B, C, D, E, F
 }
-enum RetrieveOptions
+enum RetrieveMode
 {
     highest,
     lowest,
