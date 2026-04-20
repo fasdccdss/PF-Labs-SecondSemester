91dffa1183d5737ff06e9acb4d9c2ec51d46e8c5
Tymur <nsdfd36@gmail.com>
2026-04-21 01:45:40 +0300

    Task 7 nd shi

---
+++ src/eventemitter.ts
@@ -0,0 +1,15 @@
+import { EventEmitter } from 'node:events';
+
+const emitter = new EventEmitter();
+
+export function Subscribe(command: string, handler: (args: string[]) => Promise<void>) {
+    emitter.on(command, handler);
+}
+
+export function Unsubscribe(command: string, handler: (args: string[]) => Promise<void>) {
+    emitter.off(command, handler);
+}
+
+export function Dispatch(command: string, args: string[]) {
+    emitter.emit(command, args);
+}
