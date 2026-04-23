00bbd86b7b71d637f03f73134391a121cccb18a5
Tymur <nsdfd36@gmail.com>
2026-04-24 02:53:01 +0300

    implemented different AuthMethods

--- src/authproxy.ts
+++ src/authproxy.ts
@@ -1,33 +1,54 @@
-export class AuthProxy 
-{
+export class AuthProxy {
     authMethod: AuthMethod;
 
     constructor(authMethod: AuthMethod) {
         this.authMethod = authMethod;
     }
+
 }
 
-export interface AuthMethod
-{
+export interface AuthMethod {
     GetHeaders(): Record<string, string>;
 }
 
-export class ApiKey implements AuthMethod 
-{
-    userKey: string;
-
-    constructor(userKey: string) {
-        this.userKey = userKey;
+export class ApiKey implements AuthMethod {
+    key: string;
+    headerName: string = "X-API-Key"
+    // headerName is configurable because different APIs use different key header names (e.g. "X-Auth-Token")
+    constructor(userKey: string, headerName: string) {
+        this.key = userKey;
+        if (headerName) this.headerName = headerName;
     }
 
-    GetHeaders(): Record<string, string>
-    {
-        return;
+    GetHeaders(): Record<string, string> {
+        return { [this.headerName]: this.key };
     }
 }
-export class OAuth implements AuthMethod 
-{
+export class OAuth implements AuthMethod {
+    token: string;
+
+    constructor(userToken: string) {
+        this.token = userToken;
+    }
+
     GetHeaders(): Record<string, string> {
-        return;
+        return { "Authorization": `Bearer ${this.token}` };
+    }
+}
+export class JWT implements AuthMethod {
+    token: string;
+    expireDate: number;
+    // tokenLifeTime should be typed in minutes
+    constructor(userToken: string, tokenLifeTime: number) {
+        this.token = userToken;
+        tokenLifeTime *= 60 * 1000; // convert to minutes
+        this.expireDate = Date.now() + tokenLifeTime;
+    }
+
+    GetHeaders(): Record<string, string> {
+        if (Date.now() > this.expireDate)
+            throw new Error("JWT token has expired");
+
+        return { "Authorization": `Bearer ${this.token}` };
     }
 }
